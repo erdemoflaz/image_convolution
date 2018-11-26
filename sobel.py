a2278942a81be44d28a2ef923efe25eecc631255
Erdem Oflaz <erdemoflaz4@gmail.com>
2018-11-27 02:25:01 +0300

    ✨  edit

--- sobel.py
+++ sobel.py
@@ -4,15 +4,8 @@ from math import sqrt
 input_image = Image.open("erdem.jpg")
 input_pixels = input_image.load()
 
+# renk yogunlugu bul
 intensity = [[sum(input_pixels[x, y]) / 3 for y in range(input_image.height)] for x in range(input_image.width)]
-
-# Sobel kernels
-# kernelx = [[-1, 0, 1],
-#            [-2, 0, 2],
-#            [-1, 0, 1]]
-# kernely = [[-1, -2, -1],
-#            [0, 0, 0],
-#            [1, 2, 1]]
 
 kernelx = [[1, 0, -1],
            [2, 0, -2],
@@ -21,11 +14,11 @@ kernely = [[1, 2, 1],
            [0, 0, 0],
            [-1, -2, -1]]
 
-# Create output image
+# yeni image
 output_image = Image.new("RGB", input_image.size)
 draw = ImageDraw.Draw(output_image)
 
-# Compute convolution between intensity and kernels
+# renk yogunlugu ile cekirdek arasindaki konvulusyonu hesapla
 for x in range(1, input_image.width - 1):
     for y in range(1, input_image.height - 1):
         magx, magy = 0, 0
@@ -36,7 +29,7 @@ for x in range(1, input_image.width - 1):
                 magx += intensity[xn][yn] * kernelx[a][b]
                 magy += intensity[xn][yn] * kernely[a][b]
 
-        # Draw in black and white the magnitude
+        # siyah beyaz olarak ciz
         color = int(sqrt(magx**2 + magy**2))
         draw.point((x, y), (color, color, color))
 
